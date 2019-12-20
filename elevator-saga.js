51b4c2fec92793c1122ed656e4eb3fcecfae8fff
minnypatel <minnypatel@googlemail.com>
2019-12-20 11:31:59 +0000

    remove unused parameters

--- elevator-saga.js
+++ elevator-saga.js
@@ -1,11 +1,11 @@
 {
-  init: function(elevators, floors) {
-    elevators.forEach((elevator, index) => {
+  init: function(elevators) {
+    elevators.forEach((elevator) => {
       elevator.on('idle', () => {
         elevator.goToFloor(0);
       });
 
-      elevator.on('floor_button_pressed', (floorNumber) => {
+      elevator.on('floor_button_pressed', () => {
         elevator.goToFloor(elevator.getPressedFloors()[0]);
       });
     });
